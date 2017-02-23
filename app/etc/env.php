943047ee040ed78be207f5f87c805e4109d8ed4e
thanh <huuthanhcntt3a2@gmail.com>
2017-02-23 04:32:44 +0000

    add code

---
+++ app/etc/env.php
@@ -0,0 +1,61 @@
+<?php
+return array (
+  'backend' => 
+  array (
+    'frontName' => 'admin',
+  ),
+  'install' => 
+  array (
+    'date' => 'Thu, 23 Feb 2017 04:18:18 +0000',
+  ),
+  'crypt' => 
+  array (
+    'key' => 'a4ee71ad006a9b256b66465c4a180ebb',
+  ),
+  'session' => 
+  array (
+    'save' => 'db',
+  ),
+  'db' => 
+  array (
+    'table_prefix' => '',
+    'connection' => 
+    array (
+      'default' => 
+      array (
+        'host' => 'localhost',
+        'dbname' => 'magento',
+        'username' => 'magento',
+        'password' => 'CRIbdJu1',
+        'model' => 'mysql4',
+        'engine' => 'innodb',
+        'initStatements' => 'SET NAMES utf8;',
+        'active' => '1',
+      ),
+    ),
+  ),
+  'resource' => 
+  array (
+    'default_setup' => 
+    array (
+      'connection' => 'default',
+    ),
+  ),
+  'x-frame-options' => 'SAMEORIGIN',
+  'MAGE_MODE' => 'default',
+  'cache_types' => 
+  array (
+    'config' => 1,
+    'layout' => 1,
+    'block_html' => 1,
+    'collections' => 1,
+    'reflection' => 1,
+    'db_ddl' => 1,
+    'eav' => 1,
+    'config_integration' => 1,
+    'config_integration_api' => 1,
+    'full_page' => 1,
+    'translate' => 1,
+    'config_webservice' => 1,
+  ),
+);
